16b518fc4005cec6a943f542ac1ab2a7ef94a78e
Q-T5 <bikathimartin@outlook.com>
2023-05-20 07:20:12 +0300

    Initial backend commit

---
+++ posengine/src/main/java/org/startup/posengine/PosengineApplication.java
@@ -0,0 +1,13 @@
+package org.startup.posengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosengineApplication.class, args);
+	}
+
+}
---
+++ posengine/src/test/java/org/startup/posengine/PosengineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.startup.posengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
