ff8c9c2d60434040b334c661a537a2d1474e34c7
Q-T5 <bikathimartin@outlook.com>
2023-06-06 16:10:00 +0300

    Create the SQL schema setup script

---
+++ setup.sql
@@ -0,0 +1,7 @@
+# Author(s): Bikathi Martin, Ruth Mwende
+# Date: 06-06-2023
+# Target:  MYSQL 8
+# Platforms: Linux, Windows
+
+DROP SCHEMA IF EXISTS `posengine-schema`;
+CREATE SCHEMA `posengine-schema`;
